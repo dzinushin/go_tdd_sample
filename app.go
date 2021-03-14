c897367f993071ffa290f8a71f9f3fcf83dadb1b
a.tanoshkin <alexander.tanoshkin@gmail.com>
2021-03-14 17:38:08 +0300

    Fix createProduct handler.

--- app.go
+++ app.go
@@ -141,7 +141,7 @@ func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request) {
 
 func (a *App) initializeRoutes() {
 	a.Router.HandleFunc("/products", a.getProducts).Methods("GET")
-	a.Router.HandleFunc("/products", a.createProduct).Methods("POST")
+	a.Router.HandleFunc("/product", a.createProduct).Methods("POST")
 	a.Router.HandleFunc("/product/{id:[0-9]+}", a.getProduct).Methods("GET")
 	a.Router.HandleFunc("/product/{id:[0-9]+}", a.updateProduct).Methods("PUT")
 	a.Router.HandleFunc("/product/{id:[0-9]+}", a.deleteProduct).Methods("DELETE")
